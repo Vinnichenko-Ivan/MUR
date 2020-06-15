3479728e20fba7463775e90188ef1bc20fff5ab9
nikkham <nkhamm18@mail.ru>
2020-06-15 16:33:44 +0700

    Test_1

--- Intera/mission.py
+++ Intera/mission.py
@@ -7,7 +7,7 @@ camera_1 = Vision("Front")
 camera_2 = Vision("Bottom")
 camera_1.get_frame()
 camera_1.img = camera_1.cut_frame((0, camera_1.original.shape[1] / 2), (0, camera_1.original.shape[0]))
-target_centre = (-1, -1)
+target_center = [-1, -1]
 while True:
     centers_arr = {}
     for name, value in colors.items():
@@ -15,13 +15,12 @@ while True:
         status, center, area = camera_1.search_figure_by_edge(4)
         if status is 1 and (name is "blue" or name is "red" or name is "white"):
             centers_arr[name] = center
-    center = (-1, -1)
     if list(centers_arr.keys()) is ["blue", "red", "white"] and centers_arr["blue"][1] > centers_arr["white"][1] and centers_arr["red"][0] > centers_arr["white"][0] and centers_arr["red"][0] > centers_arr["blue"][0]:
         continue
     else:
         M = cv2.moments(np.array(list(centers_arr.values())))
-        center[0] = int(M["m10"] / M["m00"])
-        center[1] = int(M["m01"] / M["m00"])
+        target_center[0] = int(M["m10"] / M["m00"])
+        target_center[1] = int(M["m01"] / M["m00"])
 
 
 
